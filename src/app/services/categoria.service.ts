10828d2bc0af93555c183b7423a3ca2202011f7f
Mario Ahumada <ahumadamob@gmail.com>
2025-07-24 21:58:03 -0300

    Parse data field in CategoriaService

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Categoria, CategoriaRequest } from '../models/categoria.model';
 
@@ -11,22 +11,32 @@ export class CategoriaService {
   constructor(private http: HttpClient) {}
 
   getCategorias(): Observable<Categoria[]> {
-    return this.http.get<Categoria[]>(`${this.baseUrl}/categorias`);
+    return this.http
+      .get<{ data: Categoria[] }>(`${this.baseUrl}/categorias`)
+      .pipe(map((resp) => resp.data));
   }
 
   getCategoria(id: number): Observable<Categoria> {
-    return this.http.get<Categoria>(`${this.baseUrl}/categorias/${id}`);
+    return this.http
+      .get<{ data: Categoria }>(`${this.baseUrl}/categorias/${id}`)
+      .pipe(map((resp) => resp.data));
   }
 
   createCategoria(req: CategoriaRequest): Observable<Categoria> {
-    return this.http.post<Categoria>(`${this.baseUrl}/categorias`, req);
+    return this.http
+      .post<{ data: Categoria }>(`${this.baseUrl}/categorias`, req)
+      .pipe(map((resp) => resp.data));
   }
 
   updateCategoria(id: number, req: CategoriaRequest): Observable<Categoria> {
-    return this.http.put<Categoria>(`${this.baseUrl}/categorias/${id}`, req);
+    return this.http
+      .put<{ data: Categoria }>(`${this.baseUrl}/categorias/${id}`, req)
+      .pipe(map((resp) => resp.data));
   }
 
   deleteCategoria(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/categorias/${id}`);
+    return this.http
+      .delete<{ data: void }>(`${this.baseUrl}/categorias/${id}`)
+      .pipe(map(() => undefined));
   }
 }
